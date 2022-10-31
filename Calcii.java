300c6b7a5d7418a12eafef35049efc3f14fbcc13
vaibhav <vaibhavrawat2003@gmail.com>
2022-10-31 11:44:58 +0530

    vs git commit

---
+++ Calcii.java
@@ -0,0 +1,10 @@
+public class Calcii{
+    int Addition(int a,int b){
+     return(a+b);
+    }
+    public static void main(String args[]){
+        Calcii c=new Calcii();
+        int r=c.Addition(10,20);
+        System.out.println(r);
+    }
+}
