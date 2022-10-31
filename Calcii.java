3a6dae42ee651f718d35f6e53dea426398f316e1
vaibhav <vaibhavrawat2003@gmail.com>
2022-10-31 11:52:36 +0530

    Multi added

--- Calcii.java
+++ Calcii.java
@@ -5,11 +5,16 @@ public class Calcii{
     int Subtract(int a,int b ){
         return(a-b);
     }
+    int Multi(int a, int b){
+        return(a*b);
+    }
     public static void main(String args[]){
         Calcii c=new Calcii();
         int r=c.Addition(10,20);
         int r2=c.Subtract(20,1);
+        int r3=c.Multi(10,2);
         System.out.println(r);
         System.out.println(r2);
+        System.out.println(r3);
     }
 }
